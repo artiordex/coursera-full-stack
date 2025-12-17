c55dc8cc18296c7c2153f709cd54c0affdf83024
artiordex <artiordex@gmail.com>
2025-12-18 00:19:18 +0900

    feat: 모듈 4 과제 작성

---
+++ html-css-js/module4-solution/script.js
@@ -0,0 +1,15 @@
+(function (global) {
+
+  var names = global.names;
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})(window);
